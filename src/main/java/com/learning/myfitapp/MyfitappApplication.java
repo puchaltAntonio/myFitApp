f781da45f5c4d5a180f9217beb3b8c78d7429cf6
toni <antpua@gmail.com>
2025-03-04 22:34:12 +0100

    First Commit

---
+++ src/main/java/com/learning/myfitapp/MyfitappApplication.java
@@ -0,0 +1,13 @@
+package com.learning.myfitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfitappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfitappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/myfitapp/MyfitappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.myfitapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfitappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
