e2fe110caa2e7dffbd512310e07647391807b55d
toni <antpua@gmail.com>
2025-03-05 22:12:54 +0100

    Set default time zone

--- src/main/java/com/learning/myfitapp/MyfitappApplication.java
+++ src/main/java/com/learning/myfitapp/MyfitappApplication.java
@@ -1,12 +1,16 @@
 package com.learning.myfitapp;
 
+import com.learning.myfitapp.common.config.TimeZoneConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class MyfitappApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone(TimeZoneConfig.DEFAULT_TIME_ZONE) );
 		SpringApplication.run(MyfitappApplication.class, args);
 	}
 
