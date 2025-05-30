73e534d34b04b1283c4ee7dbc013874610dddb0c
toni <antpua@gmail.com>
2025-05-30 11:57:17 +0200

    Add findById to exercise repository

--- src/main/java/com/learning/myfitapp/modules/exercise/application/repositories/ExerciseRepository.java
+++ src/main/java/com/learning/myfitapp/modules/exercise/application/repositories/ExerciseRepository.java
@@ -2,6 +2,10 @@ package com.learning.myfitapp.modules.exercise.application.repositories;
 
 import com.learning.myfitapp.modules.exercise.domain.models.Exercise;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ExerciseRepository {
     Exercise save(final Exercise exercise);
+    Optional<Exercise> findById(final UUID id);
 }
