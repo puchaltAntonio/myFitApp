557bac68a6b8f88273aa33716e52c34e2ea11802
toni <antpua@gmail.com>
2025-05-30 11:59:38 +0200

    Add new endpoint to get the exercise by id

--- src/main/java/com/learning/myfitapp/modules/exercise/infrastructure/rest/controllers/ExerciseRestController.java
+++ src/main/java/com/learning/myfitapp/modules/exercise/infrastructure/rest/controllers/ExerciseRestController.java
@@ -2,6 +2,7 @@ package com.learning.myfitapp.modules.exercise.infrastructure.rest.controllers;
 
 import com.learning.myfitapp.modules.exercise.application.usecases.createexercise.CreateExerciseUseCase;
 import com.learning.myfitapp.modules.exercise.application.usecases.createexercise.CreateExerciseUseCaseRequest;
+import com.learning.myfitapp.modules.exercise.application.usecases.getexercise.GetExerciseUseCase;
 import com.learning.myfitapp.modules.exercise.infrastructure.rest.mappers.ExerciseRestMapper;
 import com.learning.myfitapp.modules.exercise.infrastructure.rest.requests.ExerciseRestRequest;
 import com.learning.myfitapp.modules.exercise.infrastructure.rest.responses.ExerciseRestResponse;
@@ -11,12 +12,15 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,7 +33,11 @@ public class ExerciseRestController {
 
     public static final String CONTROLLER_PATH = "/v1/exercise";
 
+    public static final String GET_DELETE_AND_UPDATE_EXERCISE_PATH = "/{id}";
+
     private final CreateExerciseUseCase createExerciseUseCase;
+
+    private final GetExerciseUseCase getExerciseUseCase;
 
     private static final ExerciseRestMapper EXERCISE_REST_MAPPER = ExerciseRestMapper.INSTANCE;
 
@@ -58,4 +66,18 @@ public class ExerciseRestController {
                     response
             );
     }
+
+    @GetMapping(path = GET_DELETE_AND_UPDATE_EXERCISE_PATH)
+    @Operation(summary = "Get a exercise by id")
+    public ResponseEntity<ExerciseRestResponse> getExerciseById(
+          @PathVariable final UUID id
+    ) {
+        final ExerciseRestResponse response = EXERCISE_REST_MAPPER.exerciseToResponse(
+                getExerciseUseCase.getExercise(id)
+        );
+
+        return ResponseEntity.ok(response);
+    }
+
+
 }
